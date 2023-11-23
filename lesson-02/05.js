f8d5707ddce95b40e8501d0105f061d0c5b28217
boosterAlex <alex.booster.com@gmail.com>
2023-11-23 23:01:20 +0300

    second lesson fifth task

---
+++ lesson-02/05.js
@@ -0,0 +1,11 @@
+let passport = {
+    name: 'Petr',
+    surname: 'Petrov',
+};
+
+const passportCopy = { ...passport };
+
+passportCopy.name = 'Ivan';
+
+console.log(passport);
+console.log(passportCopy);
